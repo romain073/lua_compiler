3fe11d91568b724239462c1d9b2b4a8ae23caca8
Romain FONTAINE <romain.fontaine73@gmail.com>
2016-04-27 15:54:43 +0000

    Improved assignment handling

--- headers/Assign.cpp
+++ headers/Assign.cpp
@@ -26,11 +26,29 @@ class Assign: public Statement
     }
     
     void convert(BBlock** current){
-        for(size_t i = 0; i<this->variables.size(); i++){
-            string name = this->variables[i]->convert(current);
-            string val = this->values[i]->convert(current);
-          ThreeAd a(name, 'c', val, val);
-        (*current)->instructions.push_back(a);
+        if(this->variables.size()==1){
+            // var = value
+            string name = this->variables.front()->convert(current);
+            string val = this->values.front()->convert(current);
+            ThreeAd a(name, 'c', val, val);
+            (*current)->instructions.push_back(a);
+        }else{
+            // var1, var2, ... = val1, val2, ...
+            vector<string> tmp;
+            for(size_t i = 0; i<this->variables.size(); i++){
+                string tmp_name = Expression::newName();
+                tmp.push_back(tmp_name);
+                string val = this->values[i]->convert(current);
+                ThreeAd a(tmp_name, 'c', val, val);
+                (*current)->instructions.push_back(a);
+            }
+            
+            for(size_t i = 0; i<this->variables.size(); i++){
+                string tmp_name = tmp[i];
+                string name = this->variables[i]->convert(current);
+                ThreeAd a(name, 'c', tmp_name, tmp_name);
+                (*current)->instructions.push_back(a);
+            }
         }
     }
 
