caf70ebdf654080acfc25707285595d08258d6b8
Romain FONTAINE <romain.fontaine73@gmail.com>
2016-05-19 17:27:25 +0000

    Added support for else ifs

--- headers/If.cpp
+++ headers/If.cpp
@@ -8,18 +8,18 @@ class If: public Statement
 {
     public:
     Expression* condition;
-    Sequence* seq_true;
-    Sequence* seq_false;
+    Sequence* block_true;
+    Sequence* block_false;
     vector<pair<Expression*, Sequence*>> elsifs;
     If(Expression* condition, Sequence* instructions_true, Sequence* instructions_false, vector<pair<Expression*, Sequence*>> elsifs):
-        condition(condition), seq_true(instructions_true), seq_false(instructions_false), elsifs(elsifs)
+        condition(condition), block_true(instructions_true), block_false(instructions_false), elsifs(elsifs)
     {
     }
     
     ~If(){
         delete condition;
-        delete seq_true;
-        delete seq_false;
+        delete block_true;
+        delete block_false;
         
         for(auto i: elsifs){
             delete i.first;
@@ -32,23 +32,37 @@ class If: public Statement
         
         BBlock* back = new BBlock();
         
-        BBlock* trueBlock = new BBlock();
-        BBlock* trueCur = trueBlock;
-        seq_true->convert(&trueCur, functions);
-        (*current)->trueExit = trueBlock;
-        trueCur->trueExit = back;
-        /*
-        for(auto i: elsifs){
-            // i.first/ i.second
-        }
-        */
+        BBlock* true_block_begin = new BBlock();
+        BBlock* true_block_end = true_block_begin;
+        block_true->convert(&true_block_end, functions);
+        (*current)->trueExit = true_block_begin;
+        true_block_end->trueExit = back;
         
-        if(seq_false != NULL){
-            BBlock* falseBlock = new BBlock();
-            BBlock* falseCur = falseBlock;
-            seq_false->convert(&falseCur, functions);
-            (*current)->falseExit = falseBlock;
-            falseCur->trueExit = back;
+
+        
+        for(auto i: elsifs){
+            BBlock* elif_cond_begin = new BBlock();
+            BBlock* elif_cond_end = elif_cond_begin;
+            
+            i.first->convert(&elif_cond_end);
+            
+            (*current)->falseExit = elif_cond_begin;
+            BBlock* elif_block_begin = new BBlock();
+            BBlock* elif_block_end = elif_block_begin;
+            i.second->convert(&elif_block_end, functions);
+            elif_block_end->trueExit = back;
+            elif_cond_end->trueExit = elif_block_begin;
+            
+            *current = elif_cond_end;
+        }
+        
+        
+        if(block_false != NULL){
+            BBlock* false_block_begin = new BBlock();
+            BBlock* false_block_end = false_block_begin;
+            block_false->convert(&false_block_end, functions);
+            (*current)->falseExit = false_block_begin;
+            false_block_end->trueExit = back;
         }else{
             (*current)->falseExit = back;
         }
